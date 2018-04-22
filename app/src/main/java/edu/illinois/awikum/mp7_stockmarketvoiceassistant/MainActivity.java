cd25dc10da193ac7cb309374faaa3bb0278b99b1
awikum <awikum2@illinois.edu>
2018-04-21 23:14:47 -0500

    Preliminary design features, API Call backbone imported from MP6.

--- app/src/main/java/edu/illinois/awikum/mp7_stockmarketvoiceassistant/MainActivity.java
+++ app/src/main/java/edu/illinois/awikum/mp7_stockmarketvoiceassistant/MainActivity.java
@@ -2,12 +2,82 @@ package edu.illinois.awikum.mp7_stockmarketvoiceassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "MP7:Main";
+    /** Request queue for our API requests. */
+    private static RequestQueue requestQueue;
 
+    /**
+     * Run when this activity comes to the foreground.
+     *
+     * @param savedInstanceState unused
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up the queue for our API requests
+        requestQueue = Volley.newRequestQueue(this);
+    }
+
+    /**
+     * Run when this activity is no longer visible.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    /**
+     * Make a call to the Alpha Vantage API.
+     */
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" //ADD COMPANY NAME
+                    + "&apikey=" + BuildConfig.ALPHA_VANTAGE_API_KEY,
+
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            try {
+                                Log.d(TAG, response.toString(2));
+                            } catch (JSONException ignored) { }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
